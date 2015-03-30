a42dfde86aae6fb8ffcdb7dde52a0ed488c19665
thspinto <thspinto@gmail.com>
2015-03-29 22:12:53 -0300

    Changing Strings to ArtsyCoin

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -72,7 +72,7 @@ BitcoinGUI::BitcoinGUI(QWidget *parent):
     rpcConsole(0)
 {
     resize(850, 400);
-    setWindowTitle(tr("Banksycoin") + " - " + tr("Wallet"));
+    setWindowTitle(tr("ArtsyCoin") + " - " + tr("Wallet"));
 #ifndef Q_OS_MAC
     qApp->setWindowIcon(QIcon(":icons/bitcoin"));
     setWindowIcon(QIcon(":icons/bitcoin"));
@@ -217,7 +217,7 @@ void BitcoinGUI::createActions()
     tabGroup->addAction(overviewAction);
 
     sendCoinsAction = new QAction(QIcon(":/icons/send"), tr("&Send coins"), this);
-    sendCoinsAction->setToolTip(tr("Send coins to a Banksycoin address"));
+    sendCoinsAction->setToolTip(tr("Send coins to a ArtsyCoin address"));
     sendCoinsAction->setCheckable(true);
     sendCoinsAction->setShortcut(QKeySequence(Qt::ALT + Qt::Key_2));
     tabGroup->addAction(sendCoinsAction);
@@ -254,14 +254,14 @@ void BitcoinGUI::createActions()
     quitAction->setToolTip(tr("Quit application"));
     quitAction->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_Q));
     quitAction->setMenuRole(QAction::QuitRole);
-    aboutAction = new QAction(QIcon(":/icons/bitcoin"), tr("&About Banksycoin"), this);
-    aboutAction->setToolTip(tr("Show information about Banksycoin"));
+    aboutAction = new QAction(QIcon(":/icons/bitcoin"), tr("&About ArtsyCoin"), this);
+    aboutAction->setToolTip(tr("Show information about ArtsyCoin"));
     aboutAction->setMenuRole(QAction::AboutRole);
     aboutQtAction = new QAction(QIcon(":/trolltech/qmessagebox/images/qtlogo-64.png"), tr("About &Qt"), this);
     aboutQtAction->setToolTip(tr("Show information about Qt"));
     aboutQtAction->setMenuRole(QAction::AboutQtRole);
     optionsAction = new QAction(QIcon(":/icons/options"), tr("&Options..."), this);
-    optionsAction->setToolTip(tr("Modify configuration options for Banksycoin"));
+    optionsAction->setToolTip(tr("Modify configuration options for ArtsyCoin"));
     optionsAction->setMenuRole(QAction::PreferencesRole);
     toggleHideAction = new QAction(QIcon(":/icons/bitcoin"), tr("&Show / Hide"), this);
     encryptWalletAction = new QAction(QIcon(":/icons/lock_closed"), tr("&Encrypt Wallet..."), this);
@@ -278,7 +278,7 @@ void BitcoinGUI::createActions()
     miningOffAction = new QAction(QIcon(":/icons/mining_inactive"), tr("Mining Off"), this);
       miningOffAction->setStatusTip(tr("Stop Mining. May take some time to wind down."));
       miningOneAction = new QAction(QIcon(":/icons/mining_active"), tr("Start Mining"), this);
-      miningOneAction->setStatusTip(tr("Mine Banksycoin solo - not recommended!"));
+      miningOneAction->setStatusTip(tr("Mine ArtsyCoin solo - not recommended!"));
 
 
 
@@ -377,7 +377,7 @@ void BitcoinGUI::setClientModel(ClientModel *clientModel)
 #endif
             if(trayIcon)
             {
-                trayIcon->setToolTip(tr("Banksycoin client") + QString(" ") + tr("[testnet]"));
+                trayIcon->setToolTip(tr("ArtsyCoin client") + QString(" ") + tr("[testnet]"));
                 trayIcon->setIcon(QIcon(":/icons/toolbar_testnet"));
                 toggleHideAction->setIcon(QIcon(":/icons/toolbar_testnet"));
             }
@@ -442,7 +442,7 @@ void BitcoinGUI::createTrayIcon()
     trayIcon = new QSystemTrayIcon(this);
     trayIconMenu = new QMenu(this);
     trayIcon->setContextMenu(trayIconMenu);
-    trayIcon->setToolTip(tr("Banksycoin client"));
+    trayIcon->setToolTip(tr("ArtsyCoin client"));
     trayIcon->setIcon(QIcon(":/icons/toolbar"));
     connect(trayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)),
             this, SLOT(trayIconActivated(QSystemTrayIcon::ActivationReason)));
@@ -511,7 +511,7 @@ void BitcoinGUI::setNumConnections(int count)
     default: icon = ":/icons/connect_4"; break;
     }
     labelConnectionsIcon->setPixmap(QIcon(icon).pixmap(STATUSBAR_ICONSIZE,STATUSBAR_ICONSIZE));
-    labelConnectionsIcon->setToolTip(tr("%n active connection(s) to Banksycoin network", "", count));
+    labelConnectionsIcon->setToolTip(tr("%n active connection(s) to ArtsyCoin network", "", count));
 }
 
 void BitcoinGUI::setNumBlocks(int count, int nTotalBlocks)
@@ -824,7 +824,7 @@ void BitcoinGUI::dropEvent(QDropEvent *event)
         if (nValidUrisFound)
             gotoSendCoinsPage();
         else
-            notificator->notify(Notificator::Warning, tr("URI handling"), tr("URI can not be parsed! This can be caused by an invalid Banksycoin address or malformed URI parameters."));
+            notificator->notify(Notificator::Warning, tr("URI handling"), tr("URI can not be parsed! This can be caused by an invalid ArtsyCoin address or malformed URI parameters."));
     }
 
     event->acceptProposedAction();
@@ -839,7 +839,7 @@ void BitcoinGUI::handleURI(QString strURI)
         gotoSendCoinsPage();
     }
     else
-        notificator->notify(Notificator::Warning, tr("URI handling"), tr("URI can not be parsed! This can be caused by an invalid Banksycoin address or malformed URI parameters."));
+        notificator->notify(Notificator::Warning, tr("URI handling"), tr("URI can not be parsed! This can be caused by an invalid ArtsyCoin address or malformed URI parameters."));
 }
 
 void BitcoinGUI::setEncryptionStatus(int status)
@@ -944,7 +944,7 @@ void BitcoinGUI::miningOff()
 {
 
     labelMiningIcon->setPixmap(QIcon(":/icons/mining_inactive").pixmap(STATUSBAR_ICONSIZE,STATUSBAR_ICONSIZE));
-     labelMiningIcon->setToolTip(tr("Not mining BanksyCoin"));
+     labelMiningIcon->setToolTip(tr("Not mining ArtsyCoin"));
 
 
 mapArgs["-genproclimit"] = "0";
